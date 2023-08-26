38d8057e568faeb0d32208f3123a19e3c1da7636
이건우 <90121680+geonwoo1@users.noreply.github.com>
2023-08-26 15:31:57 +0900

    Delete spring/database/baro directory

--- spring/database/baro/src/main/java/baro/baro/repository/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package baro.baro.repository;
-
-import baro.baro.Entity.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-}

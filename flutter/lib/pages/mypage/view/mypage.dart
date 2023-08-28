7a690c6892ab03a519ee02b7e46fb520fc1aa5fb
j1hoon <jihoon729@kakao.com>
2023-08-28 21:28:39 +0900

    [New] BottomNavBar 컴포넌트화, MyPage 추가

---
+++ flutter/lib/pages/mypage/view/mypage.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:givoo/component/bottomnavbar.dart';
+
+void main() => runApp(MyPage());
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(backgroundColor: Colors.white, elevation: 0, actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+                color: Colors.black,
+                size: 30,
+              ))
+        ]),
+        body: Column(
+          children: [
+            SizedBox(height: 80,),
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('images/login/logo.png',
+                      width: MediaQuery.of(context).size.width * 0.45),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Baro',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 70,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('나의 활동'),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.favorite_border, size: 30,),
+                      SizedBox(width: 20,),
+                      Text('내 단체',style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.favorite_border, size: 30,),
+                      SizedBox(width: 20,),
+                      Text('기부 이력 확인',style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.favorite_border, size: 30,),
+                      SizedBox(width: 20,),
+                      Text('정기 기부 관리',style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BotNavBar(),
+      ),
+    );
+  }
+}

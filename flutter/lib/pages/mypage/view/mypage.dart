dc6db16e0934cb333a2a5f1376d2d1c4f53de45a
j1hoon <jihoon729@kakao.com>
2023-08-29 09:16:19 +0900

    [improve]Mypage Text >> Button 변경

--- flutter/lib/pages/mypage/view/mypage.dart
+++ flutter/lib/pages/mypage/view/mypage.dart
@@ -21,70 +21,107 @@ class MyPage extends StatelessWidget {
                 size: 30,
               ))
         ]),
-        body: Column(
-          children: [
-            SizedBox(height: 80,),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset('images/login/logo.png',
-                      width: MediaQuery.of(context).size.width * 0.45),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Baro',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 70,
-                  ),
-                ],
-              ),
+        body: Column(children: [
+          SizedBox(
+            height: 80,
+          ),
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('images/login/logo.png',
+                    width: MediaQuery.of(context).size.width * 0.45),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Baro',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+              ],
             ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.only(left: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text('나의 활동'),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.only(left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('나의 활동'),
+                SizedBox(
+                  height: 30,
+                ),
+                TextButtonTheme(
+                  data: TextButtonThemeData(),
+                  child: Column(
                     children: [
-                      Icon(Icons.favorite_border, size: 30,),
-                      SizedBox(width: 20,),
-                      Text('내 단체',style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-                    ],
+                      TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.favorite_border,
+                              size: 30,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              '내 단체',
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.favorite_border,
+                              size: 30,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              '기부 이력 확인',
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.favorite_border,
+                              size: 30,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              '정기 기부 관리',
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ],
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.favorite_border, size: 30,),
-                      SizedBox(width: 20,),
-                      Text('기부 이력 확인',style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.favorite_border, size: 30,),
-                      SizedBox(width: 20,),
-                      Text('정기 기부 관리',style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
-                    ],
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ]),
         bottomNavigationBar: BotNavBar(),
       ),
     );

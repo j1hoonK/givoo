3c6bc7e56c9caaf7bac24864074b2a73db0d93a8
gounwoo <gounwoo1@gmail.com>
2023-09-04 15:01:48 +0900

    #13 [style] 내 단체 리스트 글씨크기 조정 14 -> 12, 이미지 원형으로 변경

--- flutter/lib/component/orgList.dart
+++ flutter/lib/component/orgList.dart
@@ -3,8 +3,12 @@ import 'package:givoo/pages/mypage/model/MyOrgList.dart';
 
 Widget OrgList(MyOrgList myOrgList){
    return ListTile(
-      leading: FlutterLogo(),
-      title: Text(myOrgList.orgName),
+      leading: CircleAvatar(
+         backgroundImage: AssetImage("images/org/img.png"), // Text(key['title']),
+      ),
+      title: Text(myOrgList.orgName,style: TextStyle(
+         fontSize: 12,
+      ),),
       onTap: (){
          print(myOrgList.orgId);
       },

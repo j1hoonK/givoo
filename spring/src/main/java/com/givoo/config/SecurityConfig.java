f686806816f96367e321ecc61b4312eb06b337e7
gounwoo <gounwoo1@gmail.com>
2023-09-13 12:05:10 +0900

    #16 [improve] securityConfig 수정 - 6.1.2 버전에 맞게 수정

--- spring/src/main/java/com/givoo/config/SecurityConfig.java
+++ spring/src/main/java/com/givoo/config/SecurityConfig.java
@@ -1,57 +1,53 @@
 package com.givoo.config;
-
 import com.givoo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Autowired
     MemberService memberService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .loginPage("/members/login")
-                .defaultSuccessUrl("/")
-                .usernameParameter("username")
-                .failureUrl("/members/login/error")
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
-                .logoutSuccessUrl("/")
-        ;
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
-                .mvcMatchers("/", "/members/**", "/item/**","/images/**")
-                .permitAll()
-                .mvcMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated();
+        http.formLogin((formLogin) ->
+                        formLogin.loginPage("/members/login")
+                                .defaultSuccessUrl("/")
+                                .usernameParameter("username")
+                                .failureUrl("/members/login/error")
+                )
+                .logout((logout) ->
+                        logout.logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                                .logoutSuccessUrl("/"))
+                .authorizeHttpRequests((auth) ->
+                        auth.requestMatchers("/", "/members/**", "/item/**", "/images/**")
+                                .permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated());
+        return http.build();
     }
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    @Override
+    @Bean
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**","/js/**","/img/**");
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web -> web.ignoring().requestMatchers("/css/**","/js/**","/img/**"));
     }
 }

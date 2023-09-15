009f281b92988e61c118872f4ccbfaa97ab409f3
gounwoo <gounwoo1@gmail.com>
2023-09-15 10:39:46 +0900

    #10 [style] Pay - 버튼 스타일 수정

--- flutter/lib/pages/pay/view/pay.dart
+++ flutter/lib/pages/pay/view/pay.dart
@@ -9,42 +9,38 @@ class Pay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: BaseAppbar(
         title: "후원하기",
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Container(
-                height: 200,
-              ),
-              Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "후원 금액 입력",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+        child: Column(
+          children: [
+            Container(
+              height: height*0.25,
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              alignment: Alignment.topLeft,
+              child: Text(
+                "후원 금액 입력",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Form(
-                key: formKey,
-                child: Consumer<PayCategoryProvider>(
-                  builder: (context, provider, child) {
-                    return Column(
-                      children: [
-                        Stack(
+            ),
+            Form(
+              key: formKey,
+              child: Consumer<PayCategoryProvider>(
+                builder: (context, provider, child) {
+                  return Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Stack(
                           children: [
                             TextFormField(
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return "후원할 금액을 입력해주세요";
-                                }
-                                return null;
-                              },
                               onChanged: (test) {
                                 provider.selectedString(test);
                               },
@@ -71,56 +67,61 @@ class Pay extends StatelessWidget {
                             ),
                           ],
                         ),
-                        SizedBox(
-                          height: 60,
-                        ),
-                        Container(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            "후원 수단 선택",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
+                      ),
+                      SizedBox(
+                        height: height*0.1,
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          "후원 수단 선택",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        SizedBox(
-                          height: 250,
-                          child: ListView.builder(
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemCount: provider.payCategory.length,
-                              itemBuilder: (context, idx) {
-                                return RadioListTile(
-                                    title: Text("${provider.payCategory[idx]}"),
-                                    value: idx,
-                                    groupValue: provider.num,
-                                    onChanged: (value) {
-                                      provider.selectedPay(value);
-                                    });
-                              }),
-                        ),
-                        SizedBox(
-                          width: width * 1,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              if (formKey.currentState!.validate()) {}
-                            },
-                            child: Text('후원하기'),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                              provider.abc != "" && provider.num != null
-                                  ? Colors.pinkAccent
-                                  : Colors.grey,
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
+                      ),
+                      SizedBox(
+                        height: height*0.2,
+                        child: ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: provider.payCategory.length,
+                            itemBuilder: (context, idx) {
+                              return RadioListTile(
+                                  title: Text("${provider.payCategory[idx]}"),
+                                  value: idx,
+                                  groupValue: provider.num,
+                                  onChanged: (value) {
+                                    provider.selectedPay(value);
+                                  });
+                            }),
+                      ),
+                    ],
+                  );
+                },
               ),
-            ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Consumer<PayCategoryProvider>(
+        builder: (context, provider, child) {
+        return SizedBox(
+        width: width * 1,
+        height: height *0.07,
+        child: ElevatedButton(
+          onPressed: () {
+            if (formKey.currentState!.validate()) {}
+          },
+          child: Text('후원하기'),
+          style: ElevatedButton.styleFrom(
+            backgroundColor:
+            provider.abc != "" && provider.num != null
+                ? Colors.pinkAccent
+                : Colors.grey,
           ),
         ),
+      );},
       ),
     );
   }

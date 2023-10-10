e24daca9a5f95ea7bc0acf926eba74a13ff58fab
hyein <yuleumlee@naver.com>
2023-10-10 19:01:35 +0900

    #79[new]캐러샐 슬라이더 추가
    메인 페이지 기관, 단체 공지사항

--- flutter/lib/pages/mainpage/view/mainPage.dart
+++ flutter/lib/pages/mainpage/view/mainPage.dart
@@ -5,36 +5,45 @@ import 'package:givoo/provider/OrganizationProvider.dart';
 import 'package:givoo/provider/RecommendMoreProvider.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<String> orgTypeList = [
+    List<String> orgTypeList1 = [
       "국제구제",
       "자선",
       "교육문화과학",
       "경제활동",
       "환경보전",
       "권익신장",
-      "보건복지",
-      "국제교류협력",
-      "시민사회구축",
-      "기타"
     ];
-    List<String> orgImageList = [
+    List<String> orgImageList1 = [
       "images/main/1.png",
       "images/main/2.png",
       "images/main/3.png",
       "images/main/4.png",
       "images/main/5.png",
       "images/main/6.png",
+    ];
+
+    List<String> orgTypeList2 = [
+      "보건복지",
+      "국제교류협력",
+      "시민사회구축",
+      "종교",
+      "기타",
+    ];
+    List<String> orgImageList2 = [
       "images/main/7.png",
       "images/main/8.png",
       "images/main/9.png",
-      "images/main/etc.png"
+      "images/main/10.png",
+      "images/main/etc.png",
     ];
+
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
@@ -56,47 +65,93 @@ class MainPage extends StatelessWidget {
                 ),
               ),
             ),
+            // 위쪽 그리드뷰
             Container(
               margin: EdgeInsets.all(7),
-              height: height*0.33,
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: orgTypeList.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 5, //1 개의 행에 보여줄 item 개수
-                  childAspectRatio: 1 / 1.7, //item 의 가로 1, 세로 2 의 비율
-                  mainAxisSpacing: 5, //수평 Padding
-                  crossAxisSpacing: 5, //수직 Padding
-                ),
-                itemBuilder: (context, idx) {
-                  return InkWell(
-                    onTap: () {
-                      context.push("/Recommend", extra: orgTypeList[idx]);
-                      Provider.of<RecommendMoreProvider>(context, listen: false)
-                          .fetchApi(orgTypeList[idx]);
-                    },
-                    child: Card(
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: height * 0.1,
-                            child: Image.asset(orgImageList[idx]),
-                            // width: width * 0.1,
-                            //color: Colors.black,
+              height: height * 0.16, // 절반의 높이
+              child: CarouselSlider(
+                items: [
+                  for (var idx = 0; idx < orgTypeList1.length; idx++)
+                    Container(
+                      child: InkWell(
+                        onTap: () {
+                          context.push("/Recommend", extra: orgTypeList1[idx]);
+                          Provider.of<RecommendMoreProvider>(context, listen: false)
+                              .fetchApi(orgTypeList1[idx]);
+                        },
+                        child: Card(
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: height * 0.1,
+                                child: Image.asset(orgImageList1[idx]),
+                              ),
+                              SizedBox(
+                                height: height * 0.03,
+                                child: FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: Text(orgTypeList1[idx]),
+                                ),
+                              ),
+                            ],
                           ),
-                          SizedBox(
-                            height: height * 0.03,
-                            child: FittedBox(
-                                fit: BoxFit.fill,
-                                child: Text(orgTypeList[idx])),
-                          )
-                        ],
+                        ),
                       ),
                     ),
-                  );
-                },
+                ],
+                options: CarouselOptions(
+                  height: height * 0.33,
+                  autoPlay: false, // 자동 플레이 비활성화
+                  enlargeCenterPage: false,//가운데 이미지만 크게 보이는 캐러셀,, 비활성화
+                  viewportFraction: 0.24, //컨테이너 이미지 보이는 정도
+                ),
               ),
+            ),
+            // 아래쪽 그리드뷰
+            Container(
+              // 국제구제, 자산
+              margin: EdgeInsets.all(7),
+              height: height * 0.16, // 절반의 높이
+              child: CarouselSlider(
+                items: [
+                  for (var idx = 0; idx < orgTypeList2.length; idx++)
+                    Container(
+                      child: InkWell(
+                        onTap: () {
+                          context.push("/Recommend", extra: orgTypeList2[idx]);
+                          Provider.of<RecommendMoreProvider>(context, listen: false)
+                              .fetchApi(orgTypeList2[idx]);
+                        },
+                        child: Card(
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: height * 0.1,
+                                child: Image.asset(orgImageList2[idx]),
+                              ),
+                              SizedBox(
+                                height: height * 0.03,
+                                child: FittedBox(
+                                  fit: BoxFit.fill,
+                                  child: Text(orgTypeList2[idx]),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+                options: CarouselOptions(
+                  height: height * 0.33,
+                  autoPlay: false, // 자동 플레이 비활성화
+                  enlargeCenterPage: false,//가운데 이미지만 크게 보이는 캐러셀,, 비활성화
+                  viewportFraction: 0.23,//컨테이너 이미지 보이는 정도
+                ),
+              ),
+            ),
+            SizedBox(
+              height: height * 0.02, // 조절하고 싶은 간격 값 설정
             ),
             Container(
               alignment: Alignment.topLeft,
@@ -114,10 +169,10 @@ class MainPage extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.only(right: 8.0),
                     child: IconButton(
-                        onPressed: (){
-                          Provider.of<OrganizationProvider>(context, listen: false).randomOrg();
-                        },
-                        icon: Icon(Icons.refresh)
+                      onPressed: () {
+                        Provider.of<OrganizationProvider>(context, listen: false).randomOrg();
+                      },
+                      icon: Icon(Icons.refresh),
                     ),
                   ),
                 ],
@@ -132,10 +187,10 @@ class MainPage extends StatelessWidget {
                   physics: NeverScrollableScrollPhysics(),
                   itemCount: provider.randomOrgList.length,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
-                    childAspectRatio: 1 / 1.4, //item 의 가로 1, 세로 2 의 비율
-                    mainAxisSpacing: 5, //수평 Padding
-                    crossAxisSpacing: 5, //수직 Padding
+                    crossAxisCount: 3, // 3개의 행에 4개의 아이템 표시
+                    childAspectRatio: 1 / 1.4, // item 의 가로 1, 세로 2 의 비율
+                    mainAxisSpacing: 5, // 수평 Padding
+                    crossAxisSpacing: 5, // 수직 Padding
                   ),
                   itemBuilder: (context, idx) {
                     return OrgBox(
@@ -149,8 +204,20 @@ class MainPage extends StatelessWidget {
                 ),
               );
             }),
-            SizedBox(height: height * 0.03,),
-            Image.asset('images/main/b.jpg')
+            SizedBox(
+              height: height * 0.00, // 조절하고 싶은 간격 값 설정
+            ),
+            CarouselSlider(
+              items: [
+                Image.asset('images/main/b.jpg'),
+                // Add more images here
+              ],
+              options: CarouselOptions(
+                height: height * 0.4,
+                autoPlay: true, // 자동 플레이 활성화
+                enlargeCenterPage: true,
+              ),
+            ),
           ],
         ),
       ),

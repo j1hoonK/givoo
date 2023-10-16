905b78f7c898a194a3b551a110486fbef79525d9
j1hoon <jihoon729@kakao.com>
2023-10-16 18:37:36 +0900

    #10 [improve] 디테일 수정

--- flutter/lib/services/LoginService.dart
+++ flutter/lib/services/LoginService.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:givoo/config/url.dart';
 import 'package:givoo/pages/login/view/login_first.dart';
 import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;

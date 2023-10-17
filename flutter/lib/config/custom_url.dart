79ab63221edb94e519c2a163a361c084a62fff3d
summxer <luvsummxer@gmail.com>
2023-10-17 19:00:51 +0900

    #89[new]기관 공지사항
    연결은 추후에

--- flutter/lib/config/custom_url.dart
+++ flutter/lib/config/custom_url.dart
@@ -1,5 +1,5 @@
 class CustomUrl{
-  static String url= "http://172.30.1.6:1000";
+  static String url= "http://10.0.2.2:1000";
   // 172.30.1.6 : 지훈pc
   // 172.30.1.49 : 건우pc
   // 10.0.2.2 : 에뮬레이터
